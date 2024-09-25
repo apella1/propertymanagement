9f5715b8ba9a37e1dd2b6381997e17c6e169ea6c
apella1 <johncollons5@gmail.com>
2024-09-25 08:36:40 +0300

    set up project

---
+++ src/main/kotlin/me/apella/propertymanagement/PropertymanagementApplication.kt
@@ -0,0 +1,11 @@
+package me.apella.propertymanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PropertymanagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<PropertymanagementApplication>(*args)
+}
---
+++ src/test/kotlin/me/apella/propertymanagement/PropertymanagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.apella.propertymanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PropertymanagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
